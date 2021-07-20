1f1aaa35a61c5ae39091b87623c909af610c0cc5
hkidd <harrisonakidd@gmail.com>
2021-07-20 15:35:02 -0500

    Added engineer tests based on employee tests.

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,9 +1,39 @@
 //require Engineer
 const Engineer = require("../lib/Engineer");
-//create test on manager class
+//create test on Engineer class
+describe('Engineer', () => {
 
-//test to see if you can set github using our constructor
+    // returns an "engineer" based on definition
+    test('should create a new engineer object with a name, id, email, and github username', () => {
+        const engineer = new Engineer('Harry', 1, 'harry@email.com', 'hkidd');
 
-//test to see if getRole() returns "Engineer"
+        expect(engineer.name).toEqual('Harry');
+        expect(engineer.id).toEqual(1);
+        expect(engineer.email).toEqual('harry@email.com');
+        expect(engineer.github).toEqual('hkidd');
+    })
 
-//test to see if getGithub() returns github test value
+    // test if constructor can set github
+    test('should allow the constructor to set the engineer github', () => {
+        const github = 'hkidd';
+        const engineer = new Engineer('Harry', 1, 'harry@email.com', github);
+
+        expect(engineer.github).toEqual(github);
+    })
+
+    //test to see if getGithub() returns github 
+    test('getGithub() should return the engineer github', () => {
+        const github = 'hkidd';
+        const engineer = new Engineer('Harry', 1, 'harry@email.com', github);
+
+        expect(engineer.getGithub()).toEqual(github);
+    })
+
+    //test to see if getRole() returns 'Engineer' 
+    test('getRole() should return engineer', () => {
+        const engineer = new Engineer('Harry', 1, 'harry@email.com', 'hkidd');
+
+        expect(engineer.getRole()).toEqual('Engineer');
+    })
+
+});
