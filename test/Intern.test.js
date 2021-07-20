82989b794c0e6122752e51fe93ce1923a39b3166
hkidd <harrisonakidd@gmail.com>
2021-07-20 15:40:35 -0500

    Added intern tests.

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,9 +1,39 @@
 //Require Intern  
 const Intern = require("../lib/Intern");
 //create test on Intern class
+describe('Intern', () => {
 
-//test to see if you can set school using our constructor
+    // returns an "intern" based on definition
+    test('should create a new intern object with a name, id, email, and school', () => {
+        const intern = new Intern('Harry', 1, 'harry@email.com', 'UT Austin');
 
-//test to see if getRole() returns "Intern"
+        expect(intern.name).toEqual('Harry');
+        expect(intern.id).toEqual(1);
+        expect(intern.email).toEqual('harry@email.com');
+        expect(intern.school).toEqual('UT Austin');
+    })
 
-//test to see if getSchool() returns school testvalue
+    // test if constructor can set school
+    test('should allow the constructor to set the intern school', () => {
+        const school = 'hkidd';
+        const intern = new Intern('Harry', 1, 'harry@email.com', school);
+
+        expect(intern.school).toEqual(school);
+    })
+
+    //test to see if getSchool() returns school 
+    test('getSchool() should return the intern school', () => {
+        const school = 'hkidd';
+        const intern = new Intern('Harry', 1, 'harry@email.com', school);
+
+        expect(intern.getSchool()).toEqual(school);
+    })
+
+    //test to see if getRole() returns 'Intern' 
+    test('getRole() should return intern', () => {
+        const intern = new Intern('Harry', 1, 'harry@email.com', 'UT Austin');
+
+        expect(intern.getRole()).toEqual('Intern');
+    })
+
+});
