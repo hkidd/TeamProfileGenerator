7c7bb7c2df4a6665c9b5d2d8cd59ade8bf63db34
hkidd <harrisonakidd@gmail.com>
2021-07-20 19:24:54 -0500

    CSS and HTML generation changes.

--- src/page-template.js
+++ src/page-template.js
@@ -32,11 +32,11 @@ const engineerCardArray = [];
 // create manager cards
 function managerCards() {
   managerArray.forEach((manager) => {
-    managerCardArray.push(`<div class="card border-dark mb-3" style="max-width: 18rem;">
-        <div class="card-header">Manager</div>
-        <div class="card-body text-dark">
-        <h5 class="card-title">${manager.name}</h5>
-        <p class="card-text">${manager.id}<br>${manager.email}<br>${manager.officeNumber}</p>
+    managerCardArray.push(`<div class="card d-flex flex-wrap col-3 mb-3" style="max-width: 18rem">
+    <div style="text-align: center;" class="card-header cardHead"><h3>Manager</h3></div>
+    <div class="card-body text-dark cardBod">
+        <h5 class="card-title">Name: ${manager.name}</h5>
+        <p class="card-text">ID: ${manager.id}<br>E-mail: ${manager.email}<br>Office Number: ${manager.officeNumber}</p>
         </div>
         </div>`);
   });
@@ -46,11 +46,11 @@ function managerCards() {
 // create engineer cards
 function engineerCards() {
   engineerArray.forEach((engineer) => {
-    engineerCardArray.push(`<div class="card border-dark mb-3" style="max-width: 18rem;">
-            <div class="card-header">Engineer</div>
-            <div class="card-body text-dark">
-            <h5 class="card-title">${engineer.name}</h5>
-            <p class="card-text">${engineer.id}<br>${engineer.email}<br>${engineer.github}</p>
+    engineerCardArray.push(`<div class="card d-flex flex-wrap col-3 mb-3" style="max-width: 18rem">
+    <div style="text-align: center;" class="card-header cardHead"><h3>Engineer</h3></div>
+    <div class="card-body text-dark cardBod">
+            <h5 class="card-title">Name: ${engineer.name}</h5>
+            <p class="card-text">ID: ${engineer.id}<br>E-mail: ${engineer.email}<br>GitHub: ${engineer.github}</p>
             </div>
             </div>`);
   });
@@ -60,11 +60,11 @@ function engineerCards() {
 // create intern cards
 function internCards() {
   internArray.forEach((intern) => {
-    internCardArray.push(`<div class="card border-dark mb-3" style="max-width: 18rem;">
-                <div class="card-header">Intern</div>
-                <div class="card-body text-dark">
-                <h5 class="card-title">${intern.name}</h5>
-                <p class="card-text">${intern.id}<br>${intern.email}<br>${intern.school}</p>
+    internCardArray.push(`<div class="card d-flex flex-wrap col-3 mb-3" style="max-width: 18rem">
+    <div style="text-align: center;" class="card-header cardHead"><h3>Intern</h3></div>
+    <div class="card-body text-dark cardBod">
+                <h5 class="card-title">Name: ${intern.name}</h5>
+                <p class="card-text">ID: ${intern.id}<br>E-mail: ${intern.email}<br>School: ${intern.school}</p>
                 </div>
                 </div>`);
   });
@@ -79,23 +79,25 @@ function generateHTML() {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style.css" type="text/css">
     <title>Our Team</title>
   </head>
   <body>
     <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-        ${managerCardArray.join("")}
-        ${internCardArray.join("")}
-        ${engineerCardArray.join("")}
+      <h1 style="text-align: center;" class="mainHeader">Our Team</h1>
     </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      <div class="container d-flex justify-content-center flex-wrap">
+          ${managerCardArray.join("")}
+          ${engineerCardArray.join("")}
+          ${internCardArray.join("")}
+      </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
   </html>`;
 
-
-//  Create new HTML file using the above template
-  fs.writeFile("team.html", newHTML, (err) =>
+  //  Create new HTML file using the above template
+  fs.writeFile("./output/team.html", newHTML, (err) =>
     err ? console.log(err) : console.log("Success!")
   );
 }
